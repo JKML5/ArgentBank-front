1f684baf438c8089e516e2a0a24ab89f407abcef
JK <jk.floods@gmail.com>
2023-01-09 04:05:22 +0100

    User

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Account from '../components/Account';
+import Footer from "../components/Footer";
+import '../css/User.css'
 
 function User() {
+  const userAccountsData = [
+    {
+      title: 'Argent Bank Checking (x8349)',
+      amount: '2,082.79',
+      description: 'Available Balance'
+    },
+    {
+      title: 'Argent Bank Savings (x6712)',
+      amount: '10,928.42',
+      description: 'Available Balance'
+    },
+    {
+      title: 'Argent Bank Credit Card (x8349)',
+      amount: '184.30',
+      description: 'Current Balance'
+    }
+  ]
+
   return (
-    <div>User</div>
+    <>
+      <main class="main bg-dark">
+        <div class="header">
+          <h1>Welcome back<br/>Tony Jarvis!</h1>
+          <button class="edit-button">Edit Name</button>
+        </div>
+        <h2 class="sr-only">Accounts</h2>
+        {userAccountsData.map((userAccount) => (
+          <Account title={userAccount.title} amount={userAccount.amount} description={userAccount.description} />
+        ))}
+      </main>
+      <Footer />
+    </>
   )
 }
 
