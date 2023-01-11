c4b8a89287f3aefe869c27343eb1737cb4ee9539
JK <jk.floods@gmail.com>
2023-01-11 16:42:42 +0100

    mock datas

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,36 +1,13 @@
 import React from 'react';
 import '../css/Features.css';
 import FeatureItem from './FeatureItem';
+import featureItemsData from '../_mocks_/featureItemsData';
 
 function Features() {
-  const featureItemData = [
-    {
-      id: 1,
-      type: 'Chat',
-      title: 'You are our #1 priority',
-      description:
-        'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
-    },
-    {
-      id: 2,
-      type: 'Money',
-      title: 'More savings means higher rates',
-      description:
-        'The more you save with us, the higher your interest rate will be!',
-    },
-    {
-      id: 3,
-      type: 'Security',
-      title: 'Security you can trust',
-      description:
-        'We use top of the line encryption to make sure your data and money is always safe.',
-    },
-  ];
-
   return (
     <section className="features">
       <h2 className="sr-only">Features</h2>
-      {featureItemData.map((featureItem) => (
+      {featureItemsData.map((featureItem) => (
         <FeatureItem
           key={featureItem.id}
           type={featureItem.type}
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,6 +3,8 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Account from '../components/Account';
 import Footer from '../components/Footer';
+import userAccountsData from '../_mocks_/userAccountsData';
+
 import '../css/User.css';
 
 function User() {
@@ -10,27 +12,6 @@ function User() {
   if (!useSelector((state) => state.isLogged)) {
     return <Navigate to="/" />;
   }
-
-  const userAccountsData = [
-    {
-      id: 1,
-      title: 'Argent Bank Checking (x8349)',
-      amount: '2,082.79',
-      description: 'Available Balance',
-    },
-    {
-      id: 2,
-      title: 'Argent Bank Savings (x6712)',
-      amount: '10,928.42',
-      description: 'Available Balance',
-    },
-    {
-      id: 3,
-      title: 'Argent Bank Credit Card (x8349)',
-      amount: '184.30',
-      description: 'Current Balance',
-    },
-  ];
 
   return (
     <>
