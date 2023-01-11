1a1ebadcf7296117089864ec1583fb99f91cb34b
JK <jk.floods@gmail.com>
2023-01-12 00:19:49 +0100

    SignIn - use controlled form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../store';
 import Footer from '../components/Footer';
-import LoginButton from '../components/LoginButton';
 import '../css/SignIn.css';
 
 function SignIn() {
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(username, password);
+
+    // Connect the user
+    dispatch(login());
+  }
+
+  // Redirect if user is properly logged
+  if (useSelector((state) => state.isLogged)) {
+    return <Navigate to="/user" />;
+  }
+
   return (
     <>
       <main className="main bg-dark">
@@ -13,17 +33,34 @@ function SignIn() {
           <form>
             <div className="input-wrapper">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="input-wrapper">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
               <label htmlFor="remember-me">Remember me</label>
             </div>
-            <LoginButton />
+
+            <button
+              type="button"
+              className="sign-in-button"
+              onClick={handleSubmit}
+            >
+              Sign In
+            </button>
           </form>
         </section>
       </main>
